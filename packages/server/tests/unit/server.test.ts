f0b5ef9921e7d704387cd80ee6f7eb9115eb0660
Matthew Rathbone <matthew.rathbone@gmail.com>
2025-03-20 15:29:23 -0500

    test workflow

--- packages/server/tests/unit/server.test.ts
+++ packages/server/tests/unit/server.test.ts
@@ -1,6 +1,3 @@
-import { spawnSync } from 'child_process';
-import { join } from 'path';
-import { existsSync } from 'fs';
 
 describe('Server tests', () => {
   // Very basic test for server functionality
